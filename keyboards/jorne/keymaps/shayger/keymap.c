55824eb00db950ef9ee9e09313f2034a1d5ff31e
shayger <shayger751@gmail.com>
2023-10-01 18:12:01 +0300

    finish working on the shayger layout

--- keyboards/jorne/keymaps/shayger/keymap.c
+++ keyboards/jorne/keymaps/shayger/keymap.c
@@ -31,32 +31,32 @@ bool isSneaking = false;
 #define MIN_RCT MT(MOD_RCTL, KC_MINS)
 #define EQL_LAL MT(MOD_LALT, KC_EQL)
 #define BSL_RAL MT(MOD_RALT, KC_BSLS)
-#define BSP_LSH MT(MOD_LSFT, KC_BSPC)
+#define BSP_LAL MT(MOD_LALT, KC_BSPC)
 #define SPC_RSH MT(MOD_RSFT, KC_SPC)
-#define DEL_RSE LT(_RAISE, KC_DEL)
-#define TAB_RSE LT(_RAISE, KC_TAB)
+#define TAB_CTRL MT(MOD_LCTL, KC_TAB)
+#define GUI_ESC MT(MOD_RGUI, KC_ESC)
+#define SPC_RSE LT(_RAISE, KC_SPC)
 #define ENT_LWR LT(_LOWER, KC_ENT)
-#define ESC_LWR LT(_LOWER, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
-  KC_LGUI, KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, RGUI_T(KC_RBRC),
-           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
-           KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, BSL_RAL,
-                                      TAB_RSE, SPC_RSH, ENT_LWR,      ESC_LWR, BSP_LSH, DEL_RSE
+  GUI_ESC, KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, RGUI_T(KC_RBRC),
+           TAB_CTRL,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+           KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
+                                      KC_LGUI, KC_LSFT, ENT_LWR,      SPC_RSE, KC_LSFT, BSP_LAL
 ),
 
 [_LOWER] = LAYOUT(
-  _______, KC_UNDS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  F12_RGU,
+  _______, KC_UNDS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,  F12_RGU,
            PLS_LCT, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, MIN_RCT,
-           EQL_LAL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+           EQL_LAL, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,      KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
                                       _______, _______, _______,      _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT(
-  _______, KC_NUM,  KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,      KC_VOLU, KC_HOME, KC_PSCR, KC_PGUP, KC_SCRL, KC_CAPS, _______,
-           EQL_LCT, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_UP,   KC_RGHT, KC_INS,  APP_RCT,
+  _______, KC_NUM,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, _______,
+           EQL_LCT, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_UP,   KC_RGHT, KC_INS,  KC_F12,
            _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM,      KC_VOLD, KC_END,  KC_DOWN, KC_PGDN, KC_PAUS, _______,
                                       _______, _______, _______,      _______, _______, _______
 ),
@@ -248,7 +248,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
             break;
             return false;
 
-        case TAB_RSE:
+        case SPC_RSE:
 
             if (record->event.pressed) {
                 isSneaking  = true;
